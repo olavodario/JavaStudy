e35f865cea1b53b973f69fced1b90a3c3b51cb8b
Olavo Dario <168374162+olavodario@users.noreply.github.com>
2025-02-16 17:38:34 -0300

    Add files via upload

---
+++ MyFirstJavaPRJ/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+
+    public static void main(String [] args){
+
+        //THIS IS MY FIRST JAVA PROGRAM
+        /*
+        This
+        is
+        a
+        multi-line
+        comment
+         */
+
+        System.out.print("Hello Word!\n");
+        System.out.println("Now, a im a Java Back And Devoloper! HAHAHA");
+        System.out.print("If iam became a millionaire with coding, and yeah, iam gonna dominate the word!!!!");
+    }
+}
